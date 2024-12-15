455ebe6091ff737fe1fa6d38209000e7d7eaff2f
vksazonov196 <146322185+vksazonov196@users.noreply.github.com>
2024-12-15 20:43:42 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-R9CKZ81DBQ");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
